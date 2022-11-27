17627ce8d06912ce63f0423d11a02932426eb4b9
fortinj2 <fortin.jeanphilippe@gene.com>
2022-11-27 10:50:01 -0800

    Adding globals

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c("current.control.type",
+                         "current.density.type",
+                         "current.probe.type",
+                         "genderCutoff",
+                         "mouse.click.indices"))
